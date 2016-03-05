9e66e97e4acc64e4591841a1b8153487566100de
Yang Hee-cheol <Yang Hee-cheol>
2016-03-06 02:15:23 +0900

    version 1.0

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import {workspace, window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
+import {Selection, Position, DecorationRenderOptions, Range, Diagnostic, workspace, window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -11,7 +11,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "ctags" is now active!');
-
+    let ctagsController = new CtagsController(context);
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -24,10 +24,8 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World!');
 	});
     context.subscriptions.push(disposable);
-    console.log("Test..");
     
     
-    let ctagsController = new CtagsController(context);
     
 	
    
@@ -37,30 +35,142 @@ export function activate(context: vscode.ExtensionContext) {
 export function deactivate() {
 }
 
+enum Status {NONE, LOADING, INITIALIZED};
+interface Tag{
+    symbol:string,
+    file:string,
+    pattern:string;
+}
 class CtagsController{
     private _ctag_command = "ctags";
-    private _ctags_option = "-R";
-    private _ctags_tagfile = "tags";
+    private _ctags_option = "-R -F";
+    private _ctags_tagfileName = "tags";
+    private _ctags_tagpath;
+    
+    private _current_path :string;
+    private _tag_buffer:string;
+    
+    private _status : Status;
+    
+    private _tags: Map<string,Tag> = new Map<string,Tag>();
     
     constructor(context : vscode.ExtensionContext){
-        let disposable = vscode.commands.registerCommand('extension.ctags', ()=>{
-            this.start(); 
-        });
-        context.subscriptions.push(disposable);        
-    }
-    public start(){
+        this._current_path = workspace.rootPath;
+        this._status = Status.NONE;
+        this._tags.clear();
         
-        let currentPath = workspace.rootPath;
-        if(!currentPath){
-          vscode.window.showErrorMessage('Please open a folder to start CTAGS');
-          return;
-        }
-        let exec = require('child_process').exec;        
-        let command = this._ctag_command + ' ' + this._ctags_option + " ";
-        console.log('Start ctags: ' + command);
-        exec(command,{cwd:currentPath},function(err,stdout,stderr){
-            console.log(stdout)
-            vscode.window.showInformationMessage('Ctag has been finished the job.');
+        let disposable = vscode.commands.registerCommand('extension.ctag_generate', ()=>{
+            this.generate_tag(); 
         });
-    } 
+        context.subscriptions.push(disposable);
+        
+        disposable = vscode.commands.registerCommand('extension.ctag_search', ()=>{
+            this.search(); 
+        });
+        context.subscriptions.push(disposable); 
+                
+        console.log("CTag Extension has been initialized");    
+    }
+    
+    private _load_tags(){
+        this._ctags_tagpath = require('path').join(this._current_path,this._ctags_tagfileName);
+        
+        let lineReader = require('readline').createInterface({
+            input: require('fs').createReadStream(this._ctags_tagpath)
+        });
+
+        let controller :CtagsController = this;
+        lineReader.on('line', function (line) {
+            controller._status = Status.LOADING;
+            let tag:Tag = controller._extract_tag(line);
+            
+            // Todo : Proof-of-concept, first key only.
+            if(!controller._tags.has(tag.symbol)){
+                controller._tags[tag.symbol] = tag;
+            }
+        });
+        
+        lineReader.on('close', function(){
+            print_info("Tag information has been loaded. You can search tag now");
+            controller._status = Status.INITIALIZED;
+        });
+    }
+    private _extract_tag(line : string){
+        let info_array = line.split('\t');
+        let info = {
+            symbol: info_array[0],
+            file: require('path').join(this._current_path,info_array[1]),
+            pattern:info_array[2].substr(info_array[2].indexOf('^')+1,info_array[2].indexOf('$') - info_array[2].indexOf('^') - 1) 
+        };
+        
+        let prev = "";
+        let replaced = info.pattern;
+        do{
+            prev = replaced;
+            replaced = prev.replace(/\\(\$|\/|\^|\\)/,"$1");
+        }while(prev != replaced);
+        info.pattern = replaced;          
+        return info;
+    }
+
+    private _search_tag_on_doc(tag_info :Tag){
+        let controller :CtagsController = this;
+
+        workspace.openTextDocument(tag_info.file).then(function(doc){
+            window.showTextDocument(doc).then(function(editor){
+                let text : string = doc.getText();            
+                let start : Position  = doc.positionAt(text.indexOf(tag_info.pattern));
+                let end : Position = doc.positionAt(text.indexOf(tag_info.pattern) + tag_info.pattern.length);
+                let range = new Range(start,end);
+                editor.selection = new Selection(start,end);
+            })
+             
+        },function(error){
+            window.showErrorMessage("Cannot find the symbol : " + tag_info.symbol);
+        });        
+    }
+    
+    private _tag_search(targetSymbol :string){
+        let info = this._tags[targetSymbol];
+        if(info){
+            this._search_tag_on_doc(info);            
+        }else{
+            print_error("Cannot find the symbol:" + targetSymbol);
+        }
+         
+    }
+    
+    public search(){
+        if(this._status != Status.INITIALIZED){
+            print_info("Loading tag info.. Please wait for a while and try again");
+            this.generate_tag();
+            return;
+        }
+        
+        let controller :CtagsController = this;       
+            vscode.window.showInputBox("Input the symbol name").then(function(targetSymbol){
+            controller._tag_search(targetSymbol);
+        });   
+    }
+    public generate_tag(){
+        let controller :CtagsController = this;
+        
+        let exec = require('child_process').exec;   
+        let command :string = this._ctag_command + ' ' + this._ctags_option;
+        
+        //Run ctag;
+        exec(command,{cwd:controller._current_path},function(err,stdout,stderr){
+            controller._load_tags();
+        });     
+    }
+    
+
+}
+
+function print_error(msg :string){
+    window.showErrorMessage(msg);
+}
+
+function print_info(msg :string){
+    //window.showInformationMessage(msg);
 }
